59b2b66c4396a409344f06c486657e363e0406ec
MarcusViniciusRT <marcus.thomazetti@germinare.org.br>
2024-10-07 11:08:29 -0300

    Initial commit

---
+++ Rpa_BI.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+chrome_options = webdriver.ChromeOptions()
+
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+driver.get("https://app.powerbi.com/groups/me/datasets/01779d62-f86c-4644-96d9-2622477a4b39/details?experience=power-bi")
+time.sleep(10)
+
+# Login
+campo_email = driver.find_element(By.XPATH, "//input[@placeholder='Enter email']")
+
+driver.execute_script(f"arguments[0].value = '{os.getenv('EMAIL')}';", campo_email)
+
+driver.find_element(By.XPATH, "/html/body/div/div[2]/div[2]/button").click()
+
+time.sleep(7)
+
+campo_senha = driver.find_element(By.XPATH, "//*[@id='i0118']")
+
+driver.execute_script(f"arguments[0].value = '{os.getenv('SENHA')}';", campo_senha)
+
+driver.find_element(By.XPATH, "/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[5]/div/div/div/div").click()
+
+time.sleep(4)
+
+driver.find_element(By.XPATH, "/html/body/div/form/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div[2]/div/div/div[2]").click()
+
+time.sleep(12)
+
+elemento = driver.find_element(By.XPATH, '//*[@id="content"]/tri-shell/tri-item-renderer/tri-extension-page-outlet/div[2]/dataset-details-container/dataset-action-bar/action-bar/action-button[2]/button').click()
+time.sleep(3)
+
+elemento = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/div/div/div/span[1]').click()
+time.sleep(2)
+
+print("Funcionou!")
---
+++ Script_Norm.py
@@ -0,0 +1,165 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+try:
+    # Conectar ao banco de dados de origem (primeiro)
+    conexao_origem = psycopg2.connect(
+        dbname=os.getenv('DBNAME_1'),
+        user=os.getenv('USER_1'),
+        password=os.getenv('PASSWORD_1'),
+        host=os.getenv('HOST_1'),
+        port=os.getenv('PORT_1')
+    )
+
+    # Conectar ao banco de dados de destino (segundo)
+    conexao_destino = psycopg2.connect(
+        dbname=os.getenv('DBNAME_2'),
+        user=os.getenv('USER_2'),
+        password=os.getenv('PASSWORD_2'),
+        host=os.getenv('HOST_2'),
+        port=os.getenv('PORT_2')
+    )
+
+    # Criar cursores
+    cursor_origem = conexao_origem.cursor()
+    cursor_destino = conexao_destino.cursor()
+
+    # Função para transferir dados de uma tabela
+    def transferir_dados(tabela_origem, tabela_destino, tipo_categoria, colunas):
+        if tipo_categoria:
+            query_origem = f"SELECT sId, cNome, bisinactive, '{tipo_categoria}' FROM {tabela_origem} WHERE transaction_made = false;"
+            placeholders = ', '.join(['%s'] * (len(colunas)+1))
+            query_destino = f"INSERT INTO {tabela_destino} ({', '.join(colunas)}, ctipo) VALUES ({placeholders})"
+        else:
+            query_origem = f"SELECT {', '.join(colunas)} FROM {tabela_origem} WHERE transaction_made = false;"
+        
+            placeholders = ', '.join(['%s'] * len(colunas))
+            query_destino = f"INSERT INTO {tabela_destino} ({', '.join(colunas)}) VALUES ({placeholders})"
+            
+        cursor_origem.execute(query_origem)
+        dados = cursor_origem.fetchall()
+        
+        if dados:
+            print(f"Dados a inserir de {tabela_origem}:\n{dados}")
+            for linha in dados:
+                if tipo_categoria == "Curso":
+                    cursor_destino.execute(query_destino, (f'CURS_{linha[0]}', linha[1], linha[2], linha[3]))
+                elif tipo_categoria == "Produto":
+                    cursor_destino.execute(query_destino, (f'PROD_{linha[0]}', linha[1], linha[2], linha[3]))
+                else:
+                    cursor_destino.execute(query_destino, linha)
+                
+            cursor_origem.execute(query= f"UPDATE {tabela_origem} SET transaction_made = true WHERE transaction_made = false;")
+            conexao_origem.commit()
+            conexao_destino.commit()
+            print("Dados Inseridos com sucesso!")
+            
+        else:
+            print(f"Não encontramos dados para a tabela {tabela_origem}!")
+        
+
+    def transerir_dados_geral():
+        # Transferindo dados para Categoria de CategoriaProduto
+        transferir_dados(tabela_origem="CategoriaProduto", tabela_destino="categoria", tipo_categoria='Produto', colunas=["cId", "cnome", 'bisinactive'])
+
+        # Transferindo dados para Categoria de CategoriaCurso
+        transferir_dados(tabela_origem="CategoriaCurso", tabela_destino="categoria", tipo_categoria="Curso", colunas=["cId", "cnome", 'bisinactive'])
+        
+        # Transferindo dados para Plano
+        transferir_dados(tabela_origem="Plano", tabela_destino="Plano", tipo_categoria=None, colunas=["sId", "cnome", "cdescricao", "fvalor", 'bisinactive'])
+        
+        #Transferindo dados de curso
+        tabela_origem="Curso"
+        tabela_destino="Curso"
+        colunas=["sid","bstatus", "cdescricao", "cduracao", "ccertificacao", "cnome", "fvalor", "inumeroinscricao", 'bisinactive', "idcategoriacurso"]
+        
+        query_origem = f"SELECT {', '.join(colunas)} FROM {tabela_origem} WHERE transaction_made = false;"
+        cursor_origem.execute(query_origem)
+        dados = cursor_origem.fetchall()
+            
+        placeholders = ', '.join(['%s'] * len(colunas))
+        query_destino = f"INSERT INTO {tabela_destino} ({', '.join(colunas[0:9])}, idCategoria) VALUES ({placeholders})"
+            
+        if dados:
+            print(f"Dados a inserir de {tabela_origem}:\n{dados}")
+                
+            for linha in dados:
+                cursor_destino.execute(query_destino, (linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], f'CURS_{linha[9]}'))
+
+            cursor_origem.execute(f"UPDATE {tabela_origem} SET transaction_made = true WHERE transaction_made = false;")
+            conexao_origem.commit()
+            conexao_destino.commit()
+            print("Dados Inseridos com sucesso!")
+        else:
+            print(f"Não encontramos dados para a tabela {tabela_origem}!")
+        
+    def update_data(tabela_origem, tabela_destino, tipo_categoria, colunas):
+        # Construindo a query de origem
+        query_origem = f"SELECT {', '.join(colunas)} FROM {tabela_origem} WHERE transaction_made = true AND bisupdated = true;"
+        
+        cursor_origem.execute(query_origem)
+        dados = cursor_origem.fetchall()
+        
+        if dados:
+            print(f"Dados a atualizar de {tabela_origem}:\n{dados}")
+            # Para cada linha de dados encontrada
+            for linha in dados:
+                # Construindo a query de destino dependendo da existência de 'tipo_categoria'
+                if tipo_categoria:
+                    colunas.remove('sid')
+                    query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE cid = %s"
+                    cid_valor = f"{tipo_categoria[0:4].upper()}_{linha[0]}"
+                    cursor_destino.execute(query_destino, (linha[1], linha[2], cid_valor))
+                else:
+                    if tabela_destino == "Curso":
+                        colunas.remove("idcategoriacurso")
+                        colunas.append("idcategoria")
+                        query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE sId = %s"
+                        cursor_destino.execute(query_destino, (linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], f'CURS_{linha[9]}', linha[0]))
+                    else:
+                        query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE sId = %s"
+                        cursor_destino.execute(query_destino, (*linha, linha[0]))
+            
+            # Atualizando a tabela de origem
+            cursor_origem.execute(f"UPDATE {tabela_origem} SET bisupdated = false WHERE bIsupdated = true;")
+            conexao_origem.commit()
+            conexao_destino.commit()
+            print("Dados atualizados com sucesso!")
+            
+        else:
+            print(f"Não encontramos dados para atualizar na tabela {tabela_origem}!")
+
+    # Transferindo dados das tabelas do 1 para o 2
+    transerir_dados_geral()
+    
+    # Atualizando dados para Categoria de CategoriaProduto
+    update_data(tabela_origem="CategoriaProduto", tabela_destino="categoria", tipo_categoria='Produto', colunas=["sid", "cnome", 'bisinactive'])
+
+    # Atualizando dados para Categoria de CategoriaCurso
+    update_data(tabela_origem="CategoriaCurso", tabela_destino="categoria", tipo_categoria="Curso", colunas=["sid", "cnome", 'bisinactive'])
+        
+    # Atualizando dados para Plano
+    update_data(tabela_origem="Plano", tabela_destino="Plano", tipo_categoria=None, colunas=["sid", "cnome", "cdescricao", "fvalor", 'bisinactive'])
+    
+    update_data(tabela_origem="Curso",
+        tabela_destino="Curso",
+        tipo_categoria=None,
+        colunas=["sid","bstatus", "cdescricao", "cduracao", "ccertificacao", "cnome", "fvalor", "inumeroinscricao", 'bisinactive', "idcategoriacurso"])
+
+    # Commitar e fechar conexões
+    conexao_destino.commit()
+    cursor_origem.close()
+    cursor_destino.close()
+    conexao_origem.close()
+    conexao_destino.close()
+
+except Exception as error:
+    cursor_origem.close()
+    cursor_destino.close()
+    conexao_origem.close()
+    conexao_destino.close()
+    print(f"Erro recebido: {error}")
