251698bb9413bf0515c81bb228ab481f291cb6b3
MarcusViniciusRT <marcus.thomazetti@germinare.org.br>
2024-11-05 23:49:24 -0300

    Fixing code errors

--- Script_Norm.py
+++ Script_Norm.py
@@ -108,7 +108,7 @@ try:
         query_destino = f"UPDATE {tabela_destino} SET {colunas[0]} = %s WHERE sid = %s"
         
         if dados:
-            print(f"Dados a inserir de {tabela_origem}:\n{dados}")
+            print(f"Inserindo dados de {tabela_origem} em {tabela_destino}:")
                 
             for linha in dados:
                 cursor_destino.execute(query_destino, (linha))
@@ -128,23 +128,25 @@ try:
         dados = cursor_origem.fetchall()
         
         if dados:
-            print(f"Atualizando dados de {tabela_origem} em em {tabela_destino}:\n")
+            print(f"Atualizando dados de {tabela_origem} em {tabela_destino}:\n")
             # Para cada linha de dados encontrada
             for linha in dados:
                 # Construindo a query de destino dependendo da existência de 'tipo_categoria'
                 if tipo_categoria:
-                    colunas.remove('sid')
+                    if "sid" in colunas:
+                        colunas.remove('sid')
                     query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE cid = %s"
                     cid_valor = f"{tipo_categoria[0:4].upper()}_{linha[0]}"
                     cursor_destino.execute(query_destino, (linha[1], linha[2], cid_valor))
                 else:
                     if tabela_destino == "Curso" and tabela_origem == "Curso":
-                        colunas.remove("idcategoriacurso")
-                        colunas.append("idcategoria")
-                        query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE sId = %s"
+                        if "idcategoriacurso" in colunas:
+                            colunas.remove("idcategoriacurso")
+                        query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])}, idcategoria = %s WHERE sId = %s"
                         cursor_destino.execute(query_destino, (linha[0], linha[1], linha[2], linha[3], linha[4], linha[5], linha[6], linha[7], linha[8], f'CURS_{linha[9]}', linha[0]))
                     elif tabela_destino == "Curso" and tabela_origem == "MidiaCurso":
-                        colunas.remove("idCurso")
+                        if "idCurso" in colunas:
+                            colunas.remove("idCurso")
                         query_destino = f"UPDATE {tabela_destino} SET {', '.join([f'{coluna} = %s' for coluna in colunas])} WHERE sId = %s"
                         cursor_destino.execute(query_destino, (linha[1], linha[0]))
                     else:
